1adfa4e7cdff49cd33b2f6d8002625e2f5980ce3
Eda OZDEMIR <edaozdemir@live.fr>
2024-02-08 11:58:26 +0100

    more tests

--- vowelcount/Get the Middle Character/middleCharacter.test.js
+++ vowelcount/Get the Middle Character/middleCharacter.test.js
@@ -3,5 +3,12 @@ const getMiddle = require('./middleCharacter')
 describe("Sample Tests", () => {
     it("chien doit retourner i", () => {
         expect(getMiddle("chien")).toBe("i")
-    })
+    });
+    it("test doit retourner es", () => {
+        expect(getMiddle("test")).toBe("es")
+    });
+
+    it("middle doit retourner dd", () => {
+        expect(getMiddle("middle")).toBe("dd")
+    });
 })
