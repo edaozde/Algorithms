2dc7e0c19c40993ad392ffca2061a56a25672203
Eda OZDEMIR <edaozdemir@live.fr>
2024-02-08 16:10:37 +0100

    more algo

--- listfiltering/listfiltering.js
+++ listfiltering/listfiltering.js
@@ -18,3 +18,8 @@ console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
 //
 
 module.exports = filter_list;
+
+
+// function filter_list(l) {
+//     return l.filter(function(v) {return typeof v == 'number'})
+//   }
